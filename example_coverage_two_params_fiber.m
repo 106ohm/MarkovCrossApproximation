0831190d0c4cea6ec3398583c158818fdc952e89
Leonardo Robol <leo@robol.it>
2021-11-15 15:46:14 +0100

    aaa

--- example_coverage_two_params_fiber.m
+++ example_coverage_two_params_fiber.m
@@ -3,8 +3,8 @@ function v = example_battery_two_params_fiber(j, i, t, lambda1, lambda2, pi0, Q,
 switch j
     case 1
         % fiber in time
-        % w = KolmogorovODE(Q(lambda1(i(2)), lambda2(i(3))), pi0, t);
-        w = KolmogorovIntegralODE(Q(lambda1(i(2)), lambda2(i(3))), pi0, t);
+        w = KolmogorovODE(Q(lambda1(i(2)), lambda2(i(3))), pi0, t);
+        % w = KolmogorovIntegralODE(Q(lambda1(i(2)), lambda2(i(3))), pi0, t);
         v = w * r;
     case 2
         % theta 1
